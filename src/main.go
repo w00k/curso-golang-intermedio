0217c1afb05cec6de18951f1f40f246b4b9cf9e5
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-26 23:03:19 -0300

    métodos y funciones

--- src/main.go
+++ src/main.go
@@ -7,10 +7,35 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
-	fmt.Println(e)
-	e.id = 0
+	fmt.Printf("%v\n", e)
+
+	e.id = 1
 	e.name = "Francisco"
-	fmt.Println(e)
+	fmt.Printf("%v\n", e)
+
+	e.SetId(8)
+	e.SetName("w00k")
+	fmt.Printf("%v\n", e)
+
+	fmt.Println("\nvalores desde métodos get")
+	fmt.Println("id:", e.GetId())
+	fmt.Println("name:", e.GetName())
 }
