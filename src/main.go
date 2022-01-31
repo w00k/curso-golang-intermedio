19f4d4a88305dfe6daa6eb30b994c32166b95f69
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-31 12:08:35 -0300

    funciones anónimas

--- src/main.go
+++ src/main.go
@@ -1,89 +1,27 @@
 package main
 
-import "fmt"
-
-// nofiticaciones por SMS y EMAIL
-
-type INotificationFactory interface {
-	SendNotification()
-	GetSender() ISender
-}
-
-type ISender interface {
-	GetSenderMethod() string
-	GetSenderChannel() string
-}
-
-type SMSNotification struct {
-}
-
-func (SMSNotification) SendNotification() {
-	fmt.Println("Sending notification via SMS")
-}
-
-func (SMSNotification) GetSender() ISender {
-	return SMSNotificationSender{}
-}
-
-type SMSNotificationSender struct {
-}
-
-func (SMSNotificationSender) GetSenderMethod() string {
-	return "SMS"
-}
-
-func (SMSNotificationSender) GetSenderChannel() string {
-	return "Twitter"
-}
-
-type EmailNotification struct {
-}
-
-func (EmailNotification) SendNotification() {
-	fmt.Println("Sending notification via EMAIL")
-}
-
-func (EmailNotification) GetSender() ISender {
-	return EmailNotidficationSender{}
-}
-
-type EmailNotidficationSender struct{}
-
-func (EmailNotidficationSender) GetSenderMethod() string {
-	return "Email"
-}
-
-func (EmailNotidficationSender) GetSenderChannel() string {
-	return "SES"
-}
-
-func getNotificationFactory(notificationType string) (INotificationFactory, error) {
-	if notificationType == "SMS" {
-		return &SMSNotification{}, nil
-	}
-
-	if notificationType == "Email" {
-		return &EmailNotification{}, nil
-	}
-
-	return nil, fmt.Errorf("No ntification type")
-}
-
-func sendNotification(f INotificationFactory) {
-	f.SendNotification()
-}
-
-func getMethod(f INotificationFactory) {
-	fmt.Println(f.GetSender().GetSenderMethod())
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	smsFactory, _ := getNotificationFactory("SMS")
-	emailFactory, _ := getNotificationFactory("Email")
+	x := 5
 
-	sendNotification(smsFactory)
-	sendNotification(emailFactory)
+	// función anónima
+	y := func() int {
+		return x * 2
+	}()
 
-	getMethod(smsFactory)
-	getMethod(emailFactory)
+	fmt.Println("y:", y)
+
+	// función concurrente y anónima
+	c := make(chan int)
+	go func() {
+		fmt.Println("Start function")
+		time.Sleep(5 * time.Second)
+		fmt.Println("End function")
+		c <- 1
+	}()
+	<-c
 }
