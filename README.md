bff744b122e68d1e73ae78fede560a9512549366
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-31 15:35:44 -0300

    go modules y las versiones específicos de modulos

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # curso-golang-intermedio
 curso intermediode go/golang de platzi
+
+## Módulos 
+
+1. Usar módulos en Go/Golang.
+
+```bash
+go mod init github.com/user_name/module_name
+```
+
+Se genera un archivo **go.mod**. 
+
+2. Para instalar un módulo nuevo usamos el siguiente comando.
+
+```bash 
+go get github.com/user_name/module_name
+```
+
+En el **go.mod** se agregará una nueva línea y se creará un archivo **sum.go** con las versiones del módulo.
+
+3. Para eliminar dependencias que no se usan.
+
+```bash
+go mod tidy
+```
+
+4. Para usar una versión especifica del módulo, se tiene que especificar usando mod con el siguiente comando.
+
+```bash 
+go get mod https://github.com/donvito/hellomod/v2
+```
--- src/main.go
+++ src/main.go
@@ -1,39 +1,11 @@
 package main
 
-import "fmt"
-
-// funciones variádicas, son para poder enviar varios valores
-// sin necesidad de crear una función por cantidad de inputs
-// es decir sum2, sum3,sum4, etc
-// el input es tratado como un slice
-func sum(values ...int) int {
-	total := 0
-	for _, num := range values {
-		total += num
-	}
-	return total
-}
-
-// función variádica, que imprime los nombres
-func printNames(names ...string) {
-	for _, name := range names {
-		fmt.Println(name)
-	}
-}
-
-// función con retorno con nombre
-func getValues(x int) (double int, triple int, quad int) {
-	double = x * 2
-	triple = x * 3
-	quad = x * 4
-	return
-}
+import (
+	"github.com/donvito/hellomod"
+	hellomod2 "github.com/donvito/hellomod/v2"
+)
 
 func main() {
-	fmt.Println(sum(2, 3))
-	fmt.Println(sum(2, 3, 4, 5, 6, 7, 8, 9))
-
-	printNames("Francisca", "Simón", "Luca")
-
-	fmt.Println(getValues(2))
+	hellomod.SayHello()
+	hellomod2.SayHello("Platzi")
 }
