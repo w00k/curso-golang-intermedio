4a294544a4405ccf1be9fe80ca957b82e017db4d
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-31 15:03:53 -0300

    funciones variádicas y retornos con nombre -- no olvidar

--- src/main.go
+++ src/main.go
@@ -1,27 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+// funciones variádicas, son para poder enviar varios valores
+// sin necesidad de crear una función por cantidad de inputs
+// es decir sum2, sum3,sum4, etc
+// el input es tratado como un slice
+func sum(values ...int) int {
+	total := 0
+	for _, num := range values {
+		total += num
+	}
+	return total
+}
+
+// función variádica, que imprime los nombres
+func printNames(names ...string) {
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+// función con retorno con nombre
+func getValues(x int) (double int, triple int, quad int) {
+	double = x * 2
+	triple = x * 3
+	quad = x * 4
+	return
+}
 
 func main() {
-	x := 5
+	fmt.Println(sum(2, 3))
+	fmt.Println(sum(2, 3, 4, 5, 6, 7, 8, 9))
 
-	// función anónima
-	y := func() int {
-		return x * 2
-	}()
+	printNames("Francisca", "Simón", "Luca")
 
-	fmt.Println("y:", y)
-
-	// función concurrente y anónima
-	c := make(chan int)
-	go func() {
-		fmt.Println("Start function")
-		time.Sleep(5 * time.Second)
-		fmt.Println("End function")
-		c <- 1
-	}()
-	<-c
+	fmt.Println(getValues(2))
 }
