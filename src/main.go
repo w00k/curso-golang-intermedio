7663f4f86d304758f4e0c3d606305fba8fed6293
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-31 11:55:54 -0300

    implementación final del abstract factory, go permite implementar mucho patrones de diseño que están basados en polimorfismo mediante la utilización de las interfaces

--- src/main.go
+++ src/main.go
@@ -35,3 +35,55 @@ func (SMSNotificationSender) GetSenderMethod() string {
 func (SMSNotificationSender) GetSenderChannel() string {
 	return "Twitter"
 }
+
+type EmailNotification struct {
+}
+
+func (EmailNotification) SendNotification() {
+	fmt.Println("Sending notification via EMAIL")
+}
+
+func (EmailNotification) GetSender() ISender {
+	return EmailNotidficationSender{}
+}
+
+type EmailNotidficationSender struct{}
+
+func (EmailNotidficationSender) GetSenderMethod() string {
+	return "Email"
+}
+
+func (EmailNotidficationSender) GetSenderChannel() string {
+	return "SES"
+}
+
+func getNotificationFactory(notificationType string) (INotificationFactory, error) {
+	if notificationType == "SMS" {
+		return &SMSNotification{}, nil
+	}
+
+	if notificationType == "Email" {
+		return &EmailNotification{}, nil
+	}
+
+	return nil, fmt.Errorf("No ntification type")
+}
+
+func sendNotification(f INotificationFactory) {
+	f.SendNotification()
+}
+
+func getMethod(f INotificationFactory) {
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+
+func main() {
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+
+	sendNotification(smsFactory)
+	sendNotification(emailFactory)
+
+	getMethod(smsFactory)
+	getMethod(emailFactory)
+}
