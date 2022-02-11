81660e64491455ff5078bfe4a1dc558b4952cc52
Francisco Arenas <farenas@NI-NTB-120.local>
2022-02-11 16:15:30 -0300

    mock para test

--- src/main.go
+++ src/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Person struct {
 	name string
 	age  int
+	dni  string
 }
 
 type Employee struct {
-	id int
+	id       int
+	position string
 }
 
 type FullTimeEmployee struct {
@@ -40,6 +45,44 @@ func (ftEmployee FullTimeEmployee) getMessage() string {
 // getMessage para el TemporaryEmployee
 func (temporaryEmployee TemporaryEmployee) getMessage() string {
 	return "Temporary Employee"
+}
+
+// para testcase
+// GetPersonByDni simula la búsqueda en bdd de una persona por el dni
+var GetPersonByDni = func(dni string) (Person, error) {
+	time.Sleep(5 * time.Second)
+	// SELECT * FROM ...
+	return Person{}, nil
+}
+
+// para testcase
+// GetEmployeeById simula la búsqueda en bdd de un empleado por el id
+var GetEmployeeById = func(id int) (Employee, error) {
+	time.Sleep(5 * time.Second)
+	// SELECT * FROM ...
+	return Employee{}, nil
+}
+
+// para el testcase
+// GetFullTimeEmployeeById obtiene los datos de la persona y del empleado
+func GetFullTimeEmployeeById(id int, dni string) (FullTimeEmployee, error) {
+	var ftEmployee FullTimeEmployee
+
+	e, err := GetEmployeeById(id)
+	if err != nil {
+		return ftEmployee, err
+	}
+
+	ftEmployee.Employee = e
+
+	p, err := GetPersonByDni(dni)
+	if err != nil {
+		return ftEmployee, err
+	}
+
+	ftEmployee.Person = p
+
+	return ftEmployee, nil
 }
 
 // interfaces se utilizan en casi todos los patrones de diseño y también se usan para hacer  en Go/Golang
