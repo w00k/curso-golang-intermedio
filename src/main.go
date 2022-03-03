9b91b09b6c6ec56eff001ed6c65f201de66174bd
Francisco Arenas <farenas@NI-NTB-120.local>
2022-03-03 13:00:17 -0300

    Servidor web que procesa jobs que ejecutan la función fibonacci

--- src/main.go
+++ src/main.go
@@ -2,33 +2,152 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 	"time"
 )
 
-func DoSomeThing(i time.Duration, c chan<- int, param int) {
-	time.Sleep(i)
-	c <- param
+type Job struct {
+	Name   string
+	Delay  time.Duration
+	Number int
+}
+
+type Worker struct {
+	Id         int
+	JobQueue   chan Job
+	WorkerPool chan chan Job
+	QuitChan   chan bool
+}
+
+type Dispatcher struct {
+	WorkerPool chan chan Job
+	MaxWorkers int
+	JobQueue   chan Job
+}
+
+func NewWorker(id int, workerPool chan chan Job) *Worker {
+	return &Worker{
+		Id:         id,
+		JobQueue:   make(chan Job),
+		WorkerPool: workerPool,
+		QuitChan:   make(chan bool),
+	}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.JobQueue
+
+			select {
+			case job := <-w.JobQueue:
+				fmt.Printf("Worker with id %d Started\n", w.Id)
+				fib := Fibonacci(job.Number)
+				time.Sleep(job.Delay)
+				fmt.Printf("Worker with id %d Finished result %d\n", w.Id, fib)
+			case <-w.QuitChan:
+				fmt.Printf("Worker with 8d %d Stopped\n", w.Id)
+				return
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}()
+}
+
+func NewDispatcher(jobQueue chan Job, maxWorkers int) *Dispatcher {
+	worker := make(chan chan Job, maxWorkers)
+	return &Dispatcher{
+		WorkerPool: worker,
+		MaxWorkers: maxWorkers,
+		JobQueue:   jobQueue,
+	}
+}
+
+func (d *Dispatcher) Dispatch() {
+	for job := range d.JobQueue {
+		go func(job Job) {
+			workerJobQueue := <-d.WorkerPool
+			workerJobQueue <- job
+		}(job)
+	}
+}
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.MaxWorkers; i++ {
+		worker := NewWorker(i, d.WorkerPool)
+		worker.Start()
+	}
+	go d.Dispatch()
+}
+
+func RequestHandler(w http.ResponseWriter, r *http.Request, jobQueue chan Job) {
+
+	fmt.Println("In RequestHandler")
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	delay, err := time.ParseDuration(r.FormValue("delay"))
+	if err != nil {
+		http.Error(w, "Invalid delay", http.StatusBadRequest)
+		return
+	}
+
+	value, err := strconv.Atoi(r.FormValue("value"))
+	if err != nil {
+		http.Error(w, "Invalid value", http.StatusBadRequest)
+		return
+	}
+
+	name := r.FormValue("name")
+	if len(name) == 0 {
+		http.Error(w, "Invalid name", http.StatusBadRequest)
+		return
+	}
+
+	job := Job{
+		Name:   name,
+		Delay:  delay,
+		Number: value,
+	}
+
+	jobQueue <- job
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 func main() {
-	c1 := make(chan int)
-	c2 := make(chan int)
+	const (
+		maxWorkers    = 4
+		maxQuerueSize = 20
+		port          = ":8081"
+	)
 
-	d1 := 4 * time.Second
-	d2 := 2 * time.Second
+	fmt.Println("Server start ... ")
 
-	go DoSomeThing(d1, c1, 1)
-	go DoSomeThing(d2, c2, 2)
+	jobQueue := make(chan Job, maxQuerueSize)
+	dispatcher := NewDispatcher(jobQueue, maxQuerueSize)
 
-	// fmt.Println(<-c1) // queda bloqueado, independientemente de quien llegue primero
-	// fmt.Println(<-c2) // termina primero y queda a la espera que el primer print termina primero
+	dispatcher.Run()
 
-	for i := 0; i < 2; i++ {
-		select {
-		case channelMsg1 := <-c1:
-			fmt.Println(channelMsg1)
-		case channelMsg2 := <-c2:
-			fmt.Println(channelMsg2)
-		}
-	}
+	http.HandleFunc("/fib", func(w http.ResponseWriter, r *http.Request) {
+		RequestHandler(w, r, jobQueue)
+	})
+	log.Fatal(http.ListenAndServe(port, nil))
 }
